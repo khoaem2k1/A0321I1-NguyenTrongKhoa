95c5a4038f0514d322dc7280e222bad84bab0888
Your Name <nguyentrongkhoa32@gmail.com>
2021-10-11 17:56:28 +0700

    baitap

---
+++ A0721I1-NguyenTrongKhoa/src/b6_Ke_thua/thuc_hanh/he_cac_doi_tuong_hinh_hoc/lop_shape/Main.java
@@ -0,0 +1,11 @@
+package b6_Ke_thua.thuc_hanh.he_cac_doi_tuong_hinh_hoc.lop_shape;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape sh = new Shape();
+        System.out.println(sh);
+
+        sh = new Shape("red", false);
+        System.out.println(sh);
+    }
+}
---
+++ A0721I1-NguyenTrongKhoa/src/b6_Ke_thua/thuc_hanh/he_cac_doi_tuong_hinh_hoc/lop_shape/Shape.java
@@ -0,0 +1,38 @@
+package b6_Ke_thua.thuc_hanh.he_cac_doi_tuong_hinh_hoc.lop_shape;
+
+public class Shape {
+    String color = "green";
+    Boolean filled = true;
+
+    public Shape() {
+    }
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
